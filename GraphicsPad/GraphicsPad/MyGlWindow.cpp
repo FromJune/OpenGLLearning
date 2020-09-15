a02ee956d710c6392395284a16c1acfb54948486
FromJune <353335123@qq.com>
2020-09-14 22:45:33 -0600

    Draw Triangle One by One

--- GraphicsPad/GraphicsPad/MyGlWindow.cpp
+++ GraphicsPad/GraphicsPad/MyGlWindow.cpp
@@ -4,51 +4,27 @@
 #include "MyGlWindow.h"
 using namespace std;
 
-extern const char* vertexShaderCode;
-extern const char* fragmentShaderCode;
+const uint NUM_VERTRICES_PER_TRI = 3;
+const uint NUM_FLOATS_PER_VERTICE = 6;
+const float DELTA_X = 0.1f;
+const float SIZE_PER_TRI = NUM_VERTRICES_PER_TRI * NUM_FLOATS_PER_VERTICE * sizeof(float);
+uint numTri = 0;
+const uint MAX_TRI_NUM = 20;
 
 
 void sendDataToOpenGL()
 {
-	const float triangle1Depth = 0.5f;
-	const float triangle2Depth = -0.5f;
 
-	GLfloat verts[] =
-	{
-		-1.00f, +1.00f, triangle1Depth,
-		+1.0f, +0.0f, +0.0f,
-		+1.00f, +1.00f, triangle1Depth,
-		+1.0f, +0.0f, +0.0f,
-		+0.00f, -1.00f, triangle1Depth,
-		+1.0f, +0.0f, +0.0f,
-
-		+0.00f, +1.00f, triangle2Depth,
-		+0.0f, +0.0f, +1.0f,
-		+1.00f, -1.00f, triangle2Depth,
-		+0.0f, +0.0f, +1.0f,
-		-1.00f, -1.00f, triangle2Depth,
-		+0.0f, +0.0f, +1.0f,
-
-
-	};
 	GLuint vertexBufferID;
 	glGenBuffers(1, &vertexBufferID);
 	glBindBuffer(GL_ARRAY_BUFFER, vertexBufferID);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(verts), verts, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, 20 * SIZE_PER_TRI, NULL, GL_STATIC_DRAW);
 
 	glEnableVertexAttribArray(0);
 	glEnableVertexAttribArray(1);
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 6, 0);
 	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 6, (char*)(sizeof(float) * 3));
 
-	GLushort indices[] = {
-		0,1,2,
-		3,4,5
-	};
-	GLuint indexBufferID;
-	glGenBuffers(1, &indexBufferID);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBufferID);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 }
 
 bool checkStatus(GLuint objectID,
@@ -156,10 +132,33 @@ void MyGlWindow::initializeGL()
 	installShaders();
 }
 
+void sendTriangleToOpenGL()
+{
+	if (numTri == MAX_TRI_NUM)
+		return;
+
+	const GLfloat THIS_TRI_X = -1.0f + numTri * DELTA_X;
+
+	GLfloat thisTri[] = 
+	{ 
+		THIS_TRI_X, 1.0f, 0.0f,
+		1.0f, 0.0f, 0.0f,
+
+		THIS_TRI_X + DELTA_X, 1.0f, 0.0f,
+		1.0f, 0.0f, 0.0f,
+
+		THIS_TRI_X, 0.0f, 0.0f,
+		1.0f, 0.0f, 0.0f,
+	};
+
+	glBufferSubData(GL_ARRAY_BUFFER, numTri * SIZE_PER_TRI, SIZE_PER_TRI, thisTri);
+	numTri++;
+}
+
 void MyGlWindow::paintGL()
 {
-	glClear(GL_DEPTH_BUFFER_BIT);
+	glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
 	glViewport(0, 0, width(), height());
-	//glDrawArrays(GL_TRIANGLES, 0, 6);
-	glDrawElements(GL_TRIANGLES, 15, GL_UNSIGNED_SHORT, 0);
+	sendTriangleToOpenGL();
+	glDrawArrays(GL_TRIANGLES, (numTri - 1) * NUM_VERTRICES_PER_TRI, NUM_VERTRICES_PER_TRI);
 }
